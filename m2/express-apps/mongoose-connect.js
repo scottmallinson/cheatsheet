532ee8ec4eb3c9b68945239ee48cec143476d70e
Scott Mallinson <scott@scottmallinson.com>
2019-04-30 14:06:27 +0200

    Added database variable to connection, and console logs database connection status

--- m2/express-apps/mongoose-connect.js
+++ m2/express-apps/mongoose-connect.js
@@ -1,14 +1,18 @@
 // npm install mongoose
 
 const mongoose = require('mongoose');
+const database = 'database-name'
 
 // --
 
-mongoose.connect('mongodb://localhost/database-name', {
-  keepAlive: true,
-  useNewUrlParser: true,
-  reconnectTries: Number.MAX_VALUE
-});
+mongoose.connect(`mongodb://localhost/${database}`, 
+  {
+    keepAlive: true,
+    useNewUrlParser: true,
+    reconnectTries: Number.MAX_VALUE
+  })
+  .then(()=> console.log('Connected to the database'))
+  .catch(() => console.log('Unable to establish database connection'))
 
 // OR when using provess.env to get ready for deploy
 
